f3ac4881e3a399e523b07a211c71c32ae23fa13f
Jun Aruga <jaruga@redhat.com>
2021-02-22 19:56:40 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# set -eux
-set -e
+set -ex
 
 rpm -q bash
+rpm -qf /usr/bin/test
+stat /usr/bin/echo
 
 /usr/bin/test -x /usr/bin/echo || {
-  echo "[DEBUG] /usr/bin/echo not executable 1"
+  echo "[DEBUG] /usr/bin/echo not executable 1 by /usr/bin/test."
 }
 
 test -x /usr/bin/echo || {
-  echo "[DEBUG] /usr/bin/echo not executable 2"
+  echo "[DEBUG] /usr/bin/echo not executable 2 by test."
   false
 }
